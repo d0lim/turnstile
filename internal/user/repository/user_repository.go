67df6e949cd5814d87c1f417daa3dc99cdef8c93
d0lim <limdongyoung0@gmail.com>
2024-05-27 22:33:10 +0900

    refactor: id to int64

--- internal/user/repository/user_repository.go
+++ internal/user/repository/user_repository.go
@@ -4,7 +4,7 @@ import "github.com/d0lim/turnstile/internal/user/domain"
 
 type UserRepository interface {
 	CreateUser(account *domain.User) error
-	GetUserByID(id int) (*domain.User, error)
+	GetUserByID(id int64) (*domain.User, error)
 	GetUserByEmail(email string) (*domain.User, error)
-	DeleteUser(id int) error
+	DeleteUser(id int64) error
 }
