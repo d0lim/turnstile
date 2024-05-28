5d5f7bd655f1cf778845d469f69969e33b1df1e4
d0lim <limdongyoung0@gmail.com>
2024-05-29 00:22:42 +0900

    feat: add generate.sh

---
+++ scripts/generate.sh
@@ -0,0 +1,7 @@
+root_dir=$(git rev-parse --show-toplevel)
+
+go run -mod=mod entgo.io/ent/cmd/ent generate --feature sql/execquery "$root_dir/ent/schema"
+wire "$root_dir/internal/framework/wire.go"
+# swag init -d "$root_dir/cmd/server,$root_dir/internal/" -o "$root_dir/docs"
+
+echo "[INFO] go generate success"
