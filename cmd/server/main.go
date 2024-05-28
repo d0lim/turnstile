f578dc8df92fe343f86f22c07d8f80f00874eac0
d0lim <limdongyoung0@gmail.com>
2024-05-29 00:19:52 +0900

    feat: implement main

---
+++ cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/d0lim/turnstile/internal/di"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Fatalf("Error loading .env file")
+	}
+
+	app, err := di.InitializeApp()
+	if err != nil {
+		panic(err)
+	}
+
+	logrus.Fatal(app.Listen(":3000"))
+}
