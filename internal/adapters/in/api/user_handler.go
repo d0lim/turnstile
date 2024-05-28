1380f01380f94e24a3eb73628f43ebd096bae057
d0lim <limdongyoung0@gmail.com>
2024-05-29 00:19:15 +0900

    feat: implement empty UserHandler

---
+++ internal/adapters/in/api/user_handler.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/d0lim/turnstile/internal/adapters/in/config"
+	"github.com/d0lim/turnstile/internal/core/ports/in/usecase"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler struct {
+	session *config.SessionConfig
+	usecase *usecase.UserUsecase
+}
+
+func NewUserHandler(
+	session *config.SessionConfig,
+	usecase *usecase.UserUsecase,
+) *UserHandler {
+	return &UserHandler{session: session, usecase: usecase}
+}
+
+func (h *UserHandler) RedirectLoginGoogle(c *fiber.Ctx) error {
+	return nil
+}
+
+func (h *UserHandler) CallbackGoogle(c *fiber.Ctx) error {
+	return nil
+}
+
+func SetupRoutes(app *fiber.App, handler *UserHandler) {
+	app.Post("/api/v1/auth/login/google", handler.RedirectLoginGoogle)
+	app.Get("/api/v1/auth/callback/google", handler.CallbackGoogle)
+}
