cc597dda4fd51dd8ef4c3e845954218a2b330486
d0lim <limdongyoung0@gmail.com>
2024-05-31 00:20:47 +0900

    refactor: modify UserRepository ports

--- internal/core/ports/out/repository/user_repository.go
+++ internal/core/ports/out/repository/user_repository.go
@@ -5,6 +5,6 @@ import "github.com/d0lim/turnstile/internal/core/domain"
 type UserRepository interface {
 	CreateUser(account *domain.User) error
 	GetUserByID(id int64) (*domain.User, error)
-	GetUserByEmail(email string) (*domain.User, error)
+	GetUserByOAuthProviderAndEmail(oAuthProvider string, email string) (*domain.User, error)
 	DeleteUser(id int64) error
 }
