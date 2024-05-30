513c99ed615b5185d1d05bfd61e9fc5f5112543e
d0lim <limdongyoung0@gmail.com>
2024-05-31 00:22:31 +0900

    refactor: pass context

--- internal/adapters/out/db/user_repository.go
+++ internal/adapters/out/db/user_repository.go
@@ -17,19 +17,19 @@ func NewUserRepository(client *ent.Client) repository.UserRepository {
 	return &userRepository{client: client}
 }
 
-func (r *userRepository) CreateUser(user *domain.User) error {
+func (r *userRepository) CreateUser(user *domain.User, ctx context.Context) error {
 	_, err := r.client.User.Create().
 		SetOAuthID(user.OAuthId).
 		SetOAuthProvider(user.OAuthProvider).
 		SetEmail(user.Email).
 		SetName(user.Name).
 		SetNillableProfileImageURL(user.ProfileImageUrl).
-		Save(context.Background())
+		Save(ctx)
 	return err
 }
 
-func (r *userRepository) GetUserByID(id int64) (*domain.User, error) {
-	a, err := r.client.User.Get(context.Background(), id)
+func (r *userRepository) GetUserByID(id int64, ctx context.Context) (*domain.User, error) {
+	a, err := r.client.User.Get(ctx, id)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting core by ID")
 	}
@@ -44,8 +44,8 @@ func (r *userRepository) GetUserByID(id int64) (*domain.User, error) {
 	}, nil
 }
 
-func (r *userRepository) GetUserByOAuthProviderAndEmail(oAuthProvider string, email string) (*domain.User, error) {
-	u, err := r.client.User.Query().Where(user.OAuthProvider(oAuthProvider), user.Email(email)).Only(context.Background())
+func (r *userRepository) GetUserByOAuthProviderAndEmail(oAuthProvider string, email string, ctx context.Context) (*domain.User, error) {
+	u, err := r.client.User.Query().Where(user.OAuthProvider(oAuthProvider), user.Email(email)).Only(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting core by Email")
 	}
@@ -60,6 +60,6 @@ func (r *userRepository) GetUserByOAuthProviderAndEmail(oAuthProvider string, em
 	}, nil
 }
 
-func (r *userRepository) DeleteUser(id int64) error {
-	return r.client.User.DeleteOneID(id).Exec(context.Background())
+func (r *userRepository) DeleteUser(id int64, ctx context.Context) error {
+	return r.client.User.DeleteOneID(id).Exec(ctx)
 }
