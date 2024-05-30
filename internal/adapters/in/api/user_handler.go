99227e6c87df42c37ccfecb464ad7afd95442205
d0lim <limdongyoung0@gmail.com>
2024-05-30 23:38:31 +0900

    feat: implement google login

--- internal/adapters/in/api/user_handler.go
+++ internal/adapters/in/api/user_handler.go
@@ -1,27 +1,82 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/d0lim/turnstile/internal/adapters/in/api/dto"
 	"github.com/d0lim/turnstile/internal/core/ports/in/usecase"
 	"github.com/d0lim/turnstile/internal/framework/config"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"golang.org/x/oauth2"
+	"net/http"
 )
 
 type UserHandler struct {
-	session *config.SessionConfig
-	usecase *usecase.UserUsecase
+	oauthConfig *config.OAuthConfig
+	session     *config.SessionConfig
+	usecase     *usecase.UserUsecase
 }
 
 func NewUserHandler(
+	oauthConfig *config.OAuthConfig,
 	session *config.SessionConfig,
 	usecase *usecase.UserUsecase,
 ) *UserHandler {
-	return &UserHandler{session: session, usecase: usecase}
+	return &UserHandler{
+		oauthConfig: oauthConfig,
+		session:     session,
+		usecase:     usecase,
+	}
 }
 
 func (h *UserHandler) RedirectLoginGoogle(c *fiber.Ctx) error {
-	return nil
+	session, err := h.session.Store.Get(c)
+	if err != nil {
+		return err
+	}
+	state := uuid.NewString()
+	session.Set("state", state)
+	err = session.Save()
+	if err != nil {
+		return err
+	}
+
+	authCodeURL := h.oauthConfig.Google.AuthCodeURL(state, oauth2.AccessTypeOffline)
+
+	return c.Redirect(authCodeURL, http.StatusTemporaryRedirect)
 }
 
 func (h *UserHandler) CallbackGoogle(c *fiber.Ctx) error {
-	return nil
+	session, err := h.session.Store.Get(c)
+	if err != nil {
+		return err
+	}
+	state := c.Query("state")
+	code := c.Query("code")
+	savedState := session.Get("state")
+	if savedState != state {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid state")
+	}
+
+	token, err := h.oauthConfig.Google.Exchange(c.Context(), code)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	client := h.oauthConfig.Google.Client(c.Context(), token)
+	userInfo, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+	defer userInfo.Body.Close()
+
+	var user dto.GoogleUserResponse
+	if err := json.NewDecoder(userInfo.Body).Decode(&user); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	fmt.Println(user)
+
+	return c.JSON(user)
 }
