5355698132a092cc309d5f8ea1f632e39e3d3840
d0lim <limdongyoung0@gmail.com>
2024-06-04 20:51:34 +0900

    fix: DecodedToken to Token

--- internal/core/ports/out/token/token_manager.go
+++ internal/core/ports/out/token/token_manager.go
@@ -5,6 +5,6 @@ import "github.com/d0lim/turnstile/internal/core/domain"
 type TokenManager interface {
 	IssueAccessToken(sub string) (string, error)
 	IssueRefreshToken(sub string) (string, error)
-	ValidateAccessToken(token string) (domain.DecodedToken, error)
-	ValidateRefreshToken(token string) (domain.DecodedToken, error)
+	ValidateAccessToken(token string) (domain.Token, error)
+	ValidateRefreshToken(token string) (domain.Token, error)
 }
