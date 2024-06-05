f6455f2a2be8f7e2b5f4a5fbd52d7ef83a3c13c4
d0lim <limdongyoung0@gmail.com>
2024-06-05 22:48:49 +0900

    chore: wire di inject

--- internal/di/wire_gen.go
+++ internal/di/wire_gen.go
@@ -10,6 +10,7 @@ import (
 	"github.com/d0lim/turnstile/internal/adapters/in/api"
 	"github.com/d0lim/turnstile/internal/adapters/out/db"
 	"github.com/d0lim/turnstile/internal/adapters/out/db/ent"
+	"github.com/d0lim/turnstile/internal/adapters/out/jwt"
 	"github.com/d0lim/turnstile/internal/core/ports/in/usecase"
 	"github.com/d0lim/turnstile/internal/framework"
 	"github.com/d0lim/turnstile/internal/framework/config"
@@ -26,7 +27,9 @@ func InitializeApp() (*fiber.App, error) {
 		return nil, err
 	}
 	userRepository := db.NewUserRepository(client)
-	userUsecase := usecase.NewUserUsecase(userRepository)
+	jwtConfig := config.NewJwtConfig()
+	tokenManager := jwt.NewJwtTokenManager(jwtConfig)
+	userUsecase := usecase.NewUserUsecase(userRepository, tokenManager)
 	userHandler := api.NewUserHandler(oAuthConfig, sessionConfig, userUsecase)
 	app, err := framework.NewApp(userHandler)
 	if err != nil {
