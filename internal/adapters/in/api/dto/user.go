be422c5bc315c8189b5e508865a479f814029d86
d0lim <limdongyoung0@gmail.com>
2024-06-06 16:19:57 +0900

    feat: implement Authenticate

--- internal/adapters/in/api/dto/user.go
+++ internal/adapters/in/api/dto/user.go
@@ -14,3 +14,11 @@ type GoogleUserResponse struct {
 type LoginResponse struct {
 	AccessToken string `json:"access_token"`
 }
+
+type InternalUserResponse struct {
+	ID              int64   `json:"id"`
+	OAuthProvider   string  `json:"oauth_provider"`
+	Email           string  `json:"email"`
+	Name            string  `json:"name"`
+	ProfileImageUrl *string `json:"profile_image_url"`
+}
